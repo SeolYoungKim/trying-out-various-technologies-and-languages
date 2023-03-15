3039bb0bb8a8ca7611de6f456c5cabca10879970
SeolYoungKim <nasur4da@gmail.com>
2023-03-15 20:53:40 +0900

    feat: @Aspect 사용 해보기

--- technologies/aop/src/test/java/hello/aop/AopTest.java
+++ technologies/aop/src/test/java/hello/aop/AopTest.java
@@ -1,17 +1,19 @@
 package hello.aop;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
+@Import(AspectV1.class)  // 이를 이용해서 Spring bean을 등록할 수 있기도 함. 주로 configuration을 가져오는 데 사용하나, 학습할 때는 편의를 위해 사용함
 @SpringBootTest
 public class AopTest {
     @Autowired
