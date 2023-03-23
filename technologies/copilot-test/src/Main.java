a6593a0d87df25c16ebbe661e1f670df4d6c4f55
김설영 <kimsy@gimseol-yeong-ui-MacBookPro.local>
2023-03-23 18:30:47 +0900

    feat: copilot 사용 해보기

---
+++ technologies/copilot-test/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+
+
+    //how to use copilot
+    //1. select the code you want to use copilot
+    //2. press ctrl + alt + space
+    //3. select the code you want to use
+    //4. press enter
+    //5. done
+
+    
+}
