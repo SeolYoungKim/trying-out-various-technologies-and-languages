63f0fd11308ee73155110cd35d4833aa8862d3eb
김설영 <kimsy8979@mrblue.com>
2023-07-27 16:37:25 +0900

    feat: 준영속 상태 학습

--- technologies/jpa/src/test/java/hello/jpa/identifier/IdTest.java
+++ technologies/jpa/src/test/java/hello/jpa/identifier/IdTest.java
@@ -19,16 +19,35 @@ public class IdTest {
     @Transactional
     @Test
     void idTest() {
-        Author author1 = new Author("author1");
-        Long idBeforePersist = author1.getId();
+        Author author = new Author("author1");
+        Long idBeforePersist = author.getId();
         assertThat(idBeforePersist).isNull();
 
-        entityManager.persist(author1);
-        Long idAfterPersist = author1.getId();
+        entityManager.persist(author);
+        Long idAfterPersist = author.getId();
         assertThat(idAfterPersist).isNotNull();
 
+        /*
+         * 영속 상태에서 조회
+         */
+        Author findedAuthor = entityManager.find(Author.class, 1);
+        assertThat(author.getId()).isEqualTo(findedAuthor.getId());
+        assertThat(author).isSameAs(findedAuthor);
+
         entityManager.flush();
-        Long idAfterFlush = author1.getId();
+        Long idAfterFlush = author.getId();
         assertThat(idAfterFlush).isNotNull();
+
+        /*
+         * 준영속 상태로 만든 후 다시 조회
+         */
+        // 위의 author들을 준영속 상태로 만든 후
+        entityManager.clear();
+
+        // 다시 같은 author를 찾으면
+        Author findedAuthor2 = entityManager.find(Author.class, 1);
+
+        // 동일하지 않다. -> 이는 주의해야 할 사항이다. 영속성 컨텍스트 내에서 조회한 같은 엔티티들만 동일성이 보장된다.
+        assertThat(author).isNotSameAs(findedAuthor2);
     }
 }
