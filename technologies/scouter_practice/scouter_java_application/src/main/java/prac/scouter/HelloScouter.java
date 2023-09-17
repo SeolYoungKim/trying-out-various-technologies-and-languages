208d36ddb5a3f1e105695a74284a288d8175269b
SeolYoungKim <nasur4da@gmail.com>
2023-09-17 21:40:51 +0900

    feat: java agent setting

--- technologies/scouter_practice/scouter_java_application/src/main/java/prac/scouter/HelloScouter.java
+++ technologies/scouter_practice/scouter_java_application/src/main/java/prac/scouter/HelloScouter.java
@@ -1,12 +1,27 @@
 package prac.scouter;
 
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloScouter {
     @GetMapping("/")
     public String hello() {
+        List<DummyObject> dummyObjects = new ArrayList<>();
+        for (int i = 0; i < 100000; i++) {
+            // 객체를 생성해보자. GC도 측정이 되나?
+            dummyObjects.add(new DummyObject());
+        }
+
+        log.info("dummyObjects.size()={}", dummyObjects.size());
         return "hello, scouter!";
+    }
+
+    private record DummyObject() {
+
     }
 }
